56a6168dbc4dabbce0f66e8f9d053c5cc5864786
shohu33 <shohu33@gmail.com>
2022-03-16 22:54:44 +0900

    Add txwrapper-registry

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+
+import {
+	getRegistry,
+} from '@substrate/txwrapper-registry';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    const registry = getRegistry({
+      chainName: 'Updater',
+      specName: 'aaa',
+      specVersion: 1,
+      metadataRpc: `0x111`,
+    });
+    console.log(registry);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
